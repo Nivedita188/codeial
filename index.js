7100839aabd943cbd3657ac566dadb7e36ec8890
Nivedita <niveditagisg@gmail.com>
2021-08-03 21:06:40 +0530

    first commit create the directory structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+app.listen(port, function(err){
+    if (err) {
+        console.log("Error in running the server", err);
+    }
+    console.log('Yup!My Server is running on Port', port);
+});
