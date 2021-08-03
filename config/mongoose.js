8c7266f667e2ac64183bc47826968a86f2891f83
Nivedita <niveditagisg@gmail.com>
2021-08-04 00:34:14 +0530

    Setting up User Schema

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/codeial1_db');
+mongoose.connect('mongodb://localhost:27017/codeial1_db',{useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
