fb579d16733fe8e12a6e0afe86621bb8c818047b
Nivedita <niveditagisg@gmail.com>
2021-08-04 00:43:06 +0530

    Rendering Pages for Sign up and Sign in

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -4,3 +4,16 @@ module.exports.profile=function(req,res){
         title: "UsersProfile"
     });
 }
+
+// render sign in page
+module.exports.signin=function(req,res){
+    return res.render('user_sign_in', {
+        title: "Codeial|| Sign in"
+    });
+}
+// render sign up page
+module.exports.signup=function(req,res){
+    return res.render('user_sign_up', {
+        title: "Codeial|| Sign up"
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router=express.Router();
 const userController=require('../controllers/users_controllers');
 
 router.get('/profile',userController.profile);
+router.get('/sign-in',userController.signin);
+router.get('/sign-up',userController.signup);
 
 module.exports =router;
